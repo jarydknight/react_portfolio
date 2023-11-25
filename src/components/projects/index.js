9f747ae4c7e95829e26e0ec5551937b21b13656e
Jaryd Knight <jaryd.knight@hotmail.com>
2023-11-25 13:21:50 -0500

    add images for projects

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -8,37 +8,37 @@ const projectList = [
         name: "Travel Blog",
         description: "A travel blog project built using Javascript, Node.js, Handlebars, Express.js, MySQL, Sequalize ORM, Amazon S3",
         link: "https://github.com/jarydknight/travel-blog",
-        img: require("../../assets/placeholders/Placeholder.png")
+        img: require("../../assets/images/travel_blog.jpg")
     },
     {
         name: "Social Network API",
         description: "Social medial API that can complete CRUD operations on a NoSQL database using Mongoose",
         link: "https://github.com/jarydknight/social-network-api",
-        img: require("../../assets/placeholders/Placeholder.png")
+        img: require("../../assets/images/social_network_api.jpg")
     },
     {
         name: "E-commerce",
         description: "The purpose of this project is to use the ORM Sequalize (NPM package) to complete a back end for an e-commerce application and execute CRUD operations on a database",
         link: "https://github.com/jarydknight/e-commerce",
-        img: require("../../assets/placeholders/Placeholder.png")
+        img: require("../../assets/images/ecommerce.jpg")
     },
     {
         name: "Weather Dashboard",
         description: "The purpose of this project is create a weather dashboard to showcase my skills working with third party APIs.",
         link: "https://github.com/jarydknight/weather-dashboard",
-        img: require("../../assets/placeholders/Placeholder.png")
+        img: require("../../assets/images/weather_dashboard.jpg")
     },
     {
         name: "Note Taker",
         description: "The purpose of the project is to use Express to build a server for a note taker so that notes from a client can be stored in the back-end then later retireved and served back to the client",
         link: "https://github.com/jarydknight/note-taker",
-        img: require("../../assets/placeholders/Placeholder.png")
+        img: require("../../assets/images/note_taker.jpg")
     },
     {
         name: "Employee Tracker",
         description: "The purpose of this project is to build an SQL database to manage employees and a command line interface for users to easily interact with and complete CRUD operations on the database",
         link: "https://github.com/jarydknight/employee-tracker",
-        img: require("../../assets/placeholders/Placeholder.png")
+        img: require("../../assets/images/employee_tracker.jpg")
     }
 ];
 
