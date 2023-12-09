0b1ba313be45bb92b2b7f00319f44d276c291413
Jaryd Knight <jaryd.knight@hotmail.com>
2023-12-08 22:33:44 -0500

    change logo and adjust navbar

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,25 +2,29 @@ import React from "react";
 import { Container, Row, Col, Image, NavbarToggle, Nav} from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {ReactComponent as Logo } from "../../assets/icons/logoSvg.svg";
 
 function Navigation({handleNavClick}) {
   return(
     <div className="App">
       <header className="App-header">
-        <Navbar collapseOnSelect expand="lg" fixed="top" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">JK</Navbar.Brand>
-            <NavbarToggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link onClick={() => handleNavClick("Home")} href="#home">Home</Nav.Link>
+        <Navbar collapseOnSelect expand="lg" fixed="top" className="bg-body-tertiary d-block">
+          <Container className="mx-2">
+                <Navbar.Brand className="m-0" href="#home">
+                  <Logo />
+                </Navbar.Brand>
+                <NavbarToggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link onClick={() => handleNavClick("Home")} href="#home">Home</Nav.Link>
 
-                <Nav.Link onClick={() => handleNavClick("About")} href="#about">About</Nav.Link>
+                    <Nav.Link onClick={() => handleNavClick("About")} href="#about">About</Nav.Link>
                 
-                <Nav.Link onClick={() => handleNavClick("Projects")} href="#projects">Projects</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+                    <Nav.Link onClick={() => handleNavClick("Projects")} href="#projects">Projects</Nav.Link>
+
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
           </Container>
         </Navbar>
 
